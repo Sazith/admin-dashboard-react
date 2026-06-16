e325538739eda87392ca4f8ece959df7c16884b7
shyontonSazith <mdsazithshyonton@gmail.com>
2026-06-16 16:24:09 +0600

    sidebar implementation

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import {Box, IconButton, useTheme} from "@mui/material"
 import React, { useContext } from 'react'
 import { ColorModeContext, tokens } from '../../theme'
-import InputBase from "@mui/material"
+import InputBase from "@mui/material/InputBase"
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
@@ -16,6 +16,34 @@ const Topbar = () => {
     const colorMode = useContext(ColorModeContext)
   return (
    <Box display="flex" justifyContent="space-between" p={2}>
+    {/* SEARCH BAR */}
+    <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+      <InputBase sx={{ml: 2, flex:1}}  placeholder="Search"/>
+      <IconButton type="button" sx={{p: 1}} >
+        <SearchIcon/>
+      </IconButton>
+    </Box>
+
+    {/* ICONS */}
+    <Box display="flex">
+    <IconButton onClick={colorMode.toggleColorMode}>
+      {theme.palette.mode === "dark" ? (
+        <DarkModeOutlinedIcon/>
+      ) : (
+          <LightModeOutlinedIcon/>
+      )}
+    
+    </IconButton>
+    <IconButton>
+      <NotificationsOutlinedIcon/>
+    </IconButton>
+     <IconButton>
+      <SettingslinedIcon/>
+    </IconButton>
+     <IconButton>
+      <PersonOutlinedIcon/>
+    </IconButton>
+    </Box>
 
    </Box>
   )
